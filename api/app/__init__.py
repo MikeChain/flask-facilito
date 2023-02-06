4acaaedd2fce08496634dd6b716765f7123dfcb8
Miguel Ángel Cadena Sánchez <miguelangel_cadena@outlook.com>
2023-02-05 22:07:00 -0600

    Agregando endpoints

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -2,11 +2,15 @@ from flask import Flask
 from .models import db
 from .models.task import Task
 
+from .views import api_v1
+
 
 def create_app(environment):
     app = Flask(__name__)
 
     app.config.from_object(environment)
+
+    app.register_blueprint(api_v1)
 
     with app.app_context():
         db.init_app(app)
