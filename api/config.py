11ee5e32ee14096dfc8367bd36065d932357c296
Miguel Ángel Cadena Sánchez <miguelangel_cadena@outlook.com>
2023-02-05 21:25:41 -0600

    Se agrega sql y folder de modelos

--- api/config.py
+++ api/config.py
@@ -4,6 +4,8 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/api'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 config = {
