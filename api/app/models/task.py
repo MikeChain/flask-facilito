479b418aff7d2c1cbe14f0cd4c6b4401e8df6653
Miguel Ángel Cadena Sánchez <miguelangel_cadena@outlook.com>
2023-02-05 22:39:14 -0600

    Se insertan registros de prueba

--- api/app/models/task.py
+++ api/app/models/task.py
@@ -1,5 +1,6 @@
 from . import db
 
+from sqlalchemy.event import listen
 
 class Task(db.Model):
     __tablename__ = 'tasks'
@@ -12,4 +13,17 @@ class Task(db.Model):
                            default=db.func.current_timestamp())
 
     def __str__(self) -> str:
-        return self.titles
+        return self.title
+
+
+def insert_tasks(*args, **kwargs):
+    db.session.add(
+        Task(title='Title 1', description='desc', 
+             deadline='2023-02-06 12:00:00')
+    )
+    db.session.add(
+        Task(title='Title 2', description='desc 2',
+             deadline='2023-02-07 12:00:00')
+    )
+
+    db.session.commit()
